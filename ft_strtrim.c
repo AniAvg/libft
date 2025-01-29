96f25a0b524d392d1db072067bea27f5d2560f19
AniAvg <aniavg2003@gmail.com>
2025-01-29 19:05:27 +0400

    Add files via upload

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,22 +6,28 @@
 /*   By: anavagya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 16:00:52 by anavagya          #+#    #+#             */
-/*   Updated: 2025/01/29 17:29:33 by anavagya         ###   ########.fr       */
+/*   Updated: 2025/01/29 19:02:06 by anavagya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
 	int		i;
-	char	*str;
+	int		j;
 
 	i = 0;
-	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) - ft_strlen(set) + 1));
-	if (!str)
-		return (NULL):
+	j = ft_strlen(s1) - 1;
+	if (!s1 || !set)
+		return (NULL);
+	if (*s1 == '\0')
+		return (ft_substr(s1, 0, 0));
 	while (s1[i] && ft_strchr(set, s1[i]))
 		i++;
-	while (s1[i] && ft_strrchr(set, s1[i])
+	while (s1[j] && ft_strrchr(set, s1[j]))
+		j--;
+	if (i > j)
+		return (ft_substr("", 0, 0));
+	return (ft_substr(s1, i, j - i + 1));
 }
